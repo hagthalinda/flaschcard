ad7f2328620bedc68325eaa3e7d4951e730fa5ad
hagthalinda <164033741+hagthalinda@users.noreply.github.com>
2024-10-16 09:37:52 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é javascript?',
+    'O javascript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o menor pais do mundo?',
+    'O menor pais do mundo é o Vaticano'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz bom dia em Inglês?',
+    'good morning'
+)
